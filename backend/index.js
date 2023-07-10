d48354c9cf9c65a0903f46804e8ba65f1d5890c3
vishant <vishant.96.sehrawat@gmail.com>
2023-07-10 14:05:22 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,23 @@
 const express = require("express");
+var cors = require('cors')
 const { connection } = require("./config/dbConnection");
 const { userRouter } = require("./routes/user.routes");
 const { restaurantRouter } = require("./routes/restaurant.routes");
 const { orderRouter } = require("./routes/order.routes");
 require("dotenv").config();
 const app = express()
+app.use(cors())
 app.use(express.json())
-app.use("/user",userRouter)
-app.use("/restaurants",restaurantRouter)
-app.use("/orders",orderRouter)
+app.use("/user", userRouter)
+app.use("/restaurants", restaurantRouter)
+app.use("/orders", orderRouter)
 
 
-app.get("/",(req,res)=>{
-    res.send({message:"HOme route"})
+app.get("/", (req, res) => {
+    res.send({ message: "HOme route" })
 })
 
-app.listen(process.env.PORT,async(req,res)=>{
+app.listen(process.env.PORT, async (req, res) => {
     try {
         await connection
         console.log("connected to db")
